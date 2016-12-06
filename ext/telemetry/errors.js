6244ac0de507d25a0399890867c7e135f4ee58a4
Daniel Hung <daniel@kite.com>
2016-12-06 13:05:13 -0800

    make more concise

--- ext/telemetry/errors.js
+++ ext/telemetry/errors.js
@@ -4,13 +4,14 @@ module.exports = function(tracker) {
   return {
     trackUncaught: () => {
       window.onerror = (msg, url, line, col, err) => {
-        var data = {
-          msg: msg,
-          url: url,
-          line: line,
-          col: col,
-        };
-        tracker.trackEvent("uncaught error", { uncaughtError: data });
+        tracker.trackEvent("uncaught error", {
+          uncaughtError: {
+            msg: msg,
+            url: url,
+            line: line,
+            col: col,
+          }
+        });
       };
     },
     ignoreUncaught: () => {
