b6ba54f27b532610e486fb2bac32e8de914cfaa0
abe33 <cedric.nehemie@gmail.com>
2016-12-06 20:00:35 +0100

    :white_check_mark: Add tests for isKiteInstalled
    Includes changing to `fs.existsSync` instead of spawning a `ls` child
    process.
    
    Also, calling `then` on a promise already returns a new promise, and
    exception thrown in a promise trigger a rejection so there’s no need to
    wrap everything in a promise.

--- spec/state-controller-spec.js
+++ spec/state-controller-spec.js
@@ -3,17 +3,39 @@ const StateController = require('../lib/state-controller')
 
 describe('StateController', () => {
   describe('.isKiteSupported()', () => {
-    it('returns true for darwin platform', () => {
+    it('returns a resolved promise for darwin platform', () => {
       waitsForPromise(() => StateController.isKiteSupported())
     })
 
-    it('returns false for any other platform', () => {
+    it('returns a rejected promise for any other platform', () => {
       spyOn(os, 'platform').andReturn('linux')
-      waitsForPromise({shouldReject: true}, () => StateController.isKiteSupported())
+      waitsForPromise({
+        shouldReject: true
+      }, () => StateController.isKiteSupported())
     })
   })
 
-  describe('', () => {
-    
+  describe('.isKiteInstalled()', () => {
+    describe('when a file exist at the given path', () => {
+      beforeEach(() => {
+        StateController.KITE_APP_PATH = { installed: __filename }
+      })
+
+      it('returns a resolved promise', () => {
+        waitsForPromise(() => StateController.isKiteInstalled())
+      })
+    })
+
+    describe('when there is no file at the given path', () => {
+      beforeEach(() => {
+        StateController.KITE_APP_PATH = { installed: '/path/to/file.app' }
+      })
+
+      it('returns a rejected promise', () => {
+        waitsForPromise({
+          shouldReject: true
+        }, () => StateController.isKiteInstalled())
+      })
+    })
   })
 })
