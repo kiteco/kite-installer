da76300aef1744553e869c1f6c638946d34c4a56
abe33 <cedric.nehemie@gmail.com>
2017-01-18 13:25:34 +0100

    Start adding a windows support object

---
+++ spec/support/windows-spec.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+const os = require('os');
+// const https = require('https');
+// const proc = require('child_process');
+const StateController = require('../../lib/state-controller');
+// const WindowsSupport = require('../../lib/support/windows');
+
+const {
+  fakeKiteInstallPaths, /*fakeProcesses, fakeResponse,
+  withKiteInstalled, withKiteRunning, withKiteNotRunning,
+  withFakeServer,*/
+} = require('../spec-helpers.js');
+
+describe('StateController - Windows Support', () => {
+  beforeEach(() => {
+    spyOn(os, 'platform').andReturn('win32');
+    spyOn(os, 'release').andReturn('6.1.3'); // NT6.1 = Windows 7
+  });
+
+  fakeKiteInstallPaths();
+
+  describe('.isKiteSupported()', () => {
+    it('returns a resolved promise', () => {
+      waitsForPromise(() => StateController.isKiteSupported());
+    });
+
+    describe('when the os release is below 6.1', () => {
+      beforeEach(() => {
+        os.release.andReturn('6.0.4'); // NT6.0 = Windows Vista
+      });
+
+      it('returns a rejected promise', () => {
+        waitsForPromise({shouldReject: true}, () => StateController.isKiteSupported());
+      });
+    });
+  });
+});
