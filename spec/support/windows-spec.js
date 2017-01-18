49736a199ca9d63a349be909d1da28bcd1341740
abe33 <cedric.nehemie@gmail.com>
2017-01-18 15:01:54 +0100

    :white_check_mark: Add test for windows architecture

--- spec/support/windows-spec.js
+++ spec/support/windows-spec.js
@@ -17,6 +17,7 @@ describe('StateController - Windows Support', () => {
   beforeEach(() => {
     spyOn(os, 'platform').andReturn('win32');
     spyOn(os, 'release').andReturn('6.1.3'); // NT6.1 = Windows 7
+    spyOn(os, 'arch').andReturn('x64'); // NT6.1 = Windows 7
   });
 
   fakeKiteInstallPaths();
@@ -29,6 +30,16 @@ describe('StateController - Windows Support', () => {
     describe('when the os release is below 6.1', () => {
       beforeEach(() => {
         os.release.andReturn('6.0.4'); // NT6.0 = Windows Vista
+      });
+
+      it('returns a rejected promise', () => {
+        waitsForPromise({shouldReject: true}, () => StateController.isKiteSupported());
+      });
+    });
+
+    describe('when the arch is not 64bit', () => {
+      beforeEach(() => {
+        os.arch.andReturn('x86');
       });
 
       it('returns a rejected promise', () => {
