bff81aa6a0285f97b760681bf5f546b50fc52706
abe33 <cedric.nehemie@gmail.com>
2017-03-20 15:00:02 +0100

    :construction: WIP account strategy

---
+++ spec/install/account-strategy-spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const AccountStrategy = require('../../lib/install/account-strategy');
+
+describe('AccountStrategy', () => {
+  let step;
+
+  beforeEach(() => {
+    step = new AccountStrategy();
+  });
+});
