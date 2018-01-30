634b8fb79ab81ad7c394621ab231eb2ea8464893
abe33 <cedric.nehemie@gmail.com>
2018-01-30 15:08:44 +0100

    :bug: Fix tmpdir deprecations

--- spec/spec-helpers.js
+++ spec/spec-helpers.js
@@ -13,9 +13,9 @@ const InstallElement = require('../lib/elements/atom/install-element');
 // This ensure that the env variables required by the
 // windows support object are available even on another platform.
 if (os.platform() !== 'win32') {
-  process.env.TMP = os.tmpDir();
-  process.env.ProgramW6432 = os.tmpDir();
-  process.env.LOCALAPPDATA = os.tmpDir();
+  process.env.TMP = os.tmpdir();
+  process.env.ProgramW6432 = os.tmpdir();
+  process.env.LOCALAPPDATA = os.tmpdir();
 }
 
 const StateController = require('../lib/state-controller');
