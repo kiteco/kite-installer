fd7f0eecb5ff4b35f169ee4b51e27c1aaa6783de
abe33 <cedric.nehemie@gmail.com>
2018-05-29 14:58:25 +0200

    :white_check_mark: Cover more parallel steps cases

--- test/install/parallel-steps.test.js
+++ test/install/parallel-steps.test.js
@@ -1,29 +1,36 @@
 'use strict';
 
+const expect = require('expect.js');
 const sinon = require('sinon');
 const {waitsForPromise} = require('kite-connect/test/helpers/async');
 const ParallelSteps = require('../../lib/install/parallel-steps');
 
-const dummyStep = (resolve = true) => {
+const dummyStep = (resolve = true, data) => {
   return {
     start: sinon.stub().callsFake(() => {
-      return resolve ? Promise.resolve() : Promise.reject();
+      return resolve ? Promise.resolve(data) : Promise.reject();
     }),
   };
 };
 
 describe('ParallelSteps', () => {
-  let steps, step1, step2;
+  let steps, step1, step2, step3;
 
   describe('when both substeps succeeds', () => {
     beforeEach(() => {
-      step1 = dummyStep();
-      step2 = dummyStep();
-      steps = new ParallelSteps([step1, step2]);
+      step1 = dummyStep(true, {foo: 'bar'});
+      step2 = dummyStep(true);
+      step3 = dummyStep(true, {bar: 'foo'});
+      steps = new ParallelSteps([step1, step2, step3]);
     });
 
     it('resolves when both substeps are resolved', () => {
-      return waitsForPromise(() => steps.start());
+      return waitsForPromise(() => steps.start()).then(data => {
+        expect(data).to.eql({
+          foo: 'bar',
+          bar: 'foo',
+        });
+      });
     });
   });
 
@@ -36,6 +43,33 @@ describe('ParallelSteps', () => {
 
     it('rejects the whole promise', () => {
       return waitsForPromise({shouldReject: true}, () => steps.start());
+    });
+  });
+
+  describe('.getView()', () => {
+    describe('when the step has its own view', () => {
+      const view = {};
+
+      it('returns this step view', () => {
+        step1 = dummyStep();
+        step2 = dummyStep(false);
+        steps = new ParallelSteps([step1, step2], {view});
+
+        expect(steps.getView()).to.eql(view);
+      });
+    });
+
+    describe('when a sub step has a view', () => {
+      const view = {};
+
+      it('returns this step view', () => {
+        step1 = dummyStep();
+        step2 = dummyStep(false);
+        step2.view = view;
+        steps = new ParallelSteps([step1, step2]);
+
+        expect(steps.getView()).to.eql(view);
+      });
     });
   });
 });
